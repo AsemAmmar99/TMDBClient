2613a4c29bb07b91ed41c58527a53946357dc7b8
Asem Ammar <asemammar99@gmail.com>
2021-10-10 00:29:34 +0200

    Edits

--- app/src/main/java/com/scorpion_a/tmdbclient/presentation/App.kt
+++ app/src/main/java/com/scorpion_a/tmdbclient/presentation/App.kt
@@ -1,9 +1,10 @@
 package com.scorpion_a.tmdbclient.presentation
 
 import android.app.Application
+import com.scorpion_a.tmdbclient.BuildConfig
 import com.scorpion_a.tmdbclient.presentation.di.Injector
 import com.scorpion_a.tmdbclient.presentation.di.artist.ArtistSubComponent
-import com.scorpion_a.tmdbclient.presentation.di.core.AppComponent
+import com.scorpion_a.tmdbclient.presentation.di.core.*
 import com.scorpion_a.tmdbclient.presentation.di.movie.MovieSubComponent
 import com.scorpion_a.tmdbclient.presentation.di.tvShow.TvShowSubComponent
 import dagger.internal.DaggerCollections
@@ -14,19 +15,24 @@ class App: Application(), Injector {
 
     override fun onCreate() {
         super.onCreate()
-        
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(applicationContext))
+            .netModule(NetModule("https://api.themoviedb.org/3/"))
+            .remoteDataModule(RemoteDataModule(BuildConfig.API_KEY))
+            .build()
+
     }
 
     override fun createMovieSubComponent(): MovieSubComponent {
-        TODO("Not yet implemented")
+        return appComponent.movieSubComponent().create()
     }
 
     override fun createArtistSubComponent(): ArtistSubComponent {
-        TODO("Not yet implemented")
+        return appComponent.artistSubComponent().create()
     }
 
     override fun createTvShowSubComponent(): TvShowSubComponent {
-        TODO("Not yet implemented")
+        return appComponent.tvShowSubComponent().create()
     }
 
 }
--- app/src/main/java/com/scorpion_a/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/scorpion_a/tmdbclient/presentation/movie/MovieActivity.kt
@@ -2,14 +2,31 @@ package com.scorpion_a.tmdbclient.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.scorpion_a.tmdbclient.R
 import com.scorpion_a.tmdbclient.databinding.ActivityMovieBinding
+import com.scorpion_a.tmdbclient.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+    lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+
+        movieViewModel = ViewModelProvider(this, factory)
+            .get(MovieViewModel::class.java)
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer{
+            Log.i("MyTag", it.toString)
+        })
     }
 }

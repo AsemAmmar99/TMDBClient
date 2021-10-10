5fe5eae5f294f0f14dc47dc3c2e43da340f54b67
Asem Ammar <asemammar99@gmail.com>
2021-10-10 03:53:10 +0200

    Edits

--- app/src/main/java/com/scorpion_a/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/scorpion_a/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,9 +3,13 @@ package com.scorpion_a.tmdbclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.scorpion_a.tmdbclient.R
 import com.scorpion_a.tmdbclient.databinding.ActivityMovieBinding
 import com.scorpion_a.tmdbclient.presentation.di.Injector
@@ -16,6 +20,7 @@ class MovieActivity : AppCompatActivity() {
     lateinit var factory: MovieViewModelFactory
     lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
@@ -24,9 +29,29 @@ class MovieActivity : AppCompatActivity() {
 
         movieViewModel = ViewModelProvider(this, factory)
             .get(MovieViewModel::class.java)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies(){
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer{
-            Log.i("MyTag", it.toString())
+            if (it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "Failed to load data..", Toast.LENGTH_LONG).show()
+            }
         })
     }
+
 }
---
+++ app/src/main/java/com/scorpion_a/tmdbclient/presentation/movie/MovieAdapter.kt
@@ -0,0 +1,53 @@
+package com.scorpion_a.tmdbclient.presentation.movie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.scorpion_a.tmdbclient.R
+import com.scorpion_a.tmdbclient.data.model.movie.Movie
+import com.scorpion_a.tmdbclient.databinding.ListItemBinding
+
+
+class MovieAdapter(): RecyclerView.Adapter<MyViewHolder>() {
+    val movieList = ArrayList<Movie>()
+
+    fun setList(movies: List<Movie>){
+        movieList.clear()
+        movieList.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(movieList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+}
+
+class MyViewHolder(val binding: ListItemBinding):
+RecyclerView.ViewHolder(binding.root){
+
+    fun bind(movie: Movie){
+        binding.titleTextView.text = movie.title
+        binding.descriptionTextView.text = movie.overview
+        val posterURL = "https://image.tmdb.org/t/p/w500"+movie.posterPath
+        Glide.with(binding.imageView.context)
+            .load(posterURL)
+            .into(binding.imageView)
+    }
+
+}
